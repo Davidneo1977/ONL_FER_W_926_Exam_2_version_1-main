613dcab9ddd71a9a7b69be49ec43777f16de4e54
Davidneo <davidneo@seznam.cz>
2024-09-15 16:14:16 +0200

    MEZERA

--- 01_Task_1/js/app.js
+++ 01_Task_1/js/app.js
@@ -3,8 +3,11 @@ function withouts(data, ...valuesToRemove) {
    return data.filter(item => !valuesToRemove.includes(item));
   }
   
+
+  
   const data = [1, 2, 3, 4, 5];
   const result = withouts(numbers, 2, 4, 6);
   
   console.log(result);
   
+
