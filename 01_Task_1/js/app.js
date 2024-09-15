69ff68812683f1bef409f0365f43cf01c348ca26
Davidneo <davidneo@seznam.cz>
2024-09-15 16:17:59 +0200

    REŠENÍ dAVID

--- 01_Task_1/js/app.js
+++ 01_Task_1/js/app.js
@@ -3,8 +3,6 @@ function withouts(data, ...valuesToRemove) {
    return data.filter(item => !valuesToRemove.includes(item));
   }
   
-
-  
   const data = [1, 2, 3, 4, 5];
   const result = withouts(numbers, 2, 4, 6);
   
